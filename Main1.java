5431b252378fcc0820f4ed43bd0e63e1d32609ed
Ayan Maharramova <amaharramova17296@ada.edu.az>
2026-03-06 11:03:00 +0400

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,34 @@
+class Rectangle {
+    int width, height;
+
+    public Rectangle(int w, int h) {
+        width = w;
+        height = h;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Rectangle rect = (Rectangle) obj;
+        return this.width == rect.width && this.height == rect.height;
+    }
+
+}
+
+//exercise 3
+class Square extends Rectangle {
+
+    public Square(int side) {
+        super(side, side);
+    }
+}
+
+public class Main1 {
+    public static void main(String[] args) {
+        Object o1 = new Rectangle(5, 10);
+        Object o2 = new Rectangle(15, 15);
+        Object o3 = new Square(15);
+
+        System.out.println("Objects are identical: " + o1.equals(o2));
+        System.out.println("Objects are identical: " + o1.equals(o3));
+        System.out.println("Objects are identical: " + o2.equals(o3));
+    } }
