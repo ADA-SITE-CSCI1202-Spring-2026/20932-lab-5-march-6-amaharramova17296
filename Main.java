72b7af6da874316a97d7e9a0fc015a722a6a7bf5
Ayan Maharramova <amaharramova17296@ada.edu.az>
2026-03-06 10:55:18 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,34 +1,60 @@
-class Rectangle {
-    int width, height;
-
-    public Rectangle(int w, int h) {
-        width = w;
-        height = h;
-    }
-
-    @Override
-    public boolean equals(Object obj1) {
-        Rectangle r =  (Rectangle)obj1;
-        return this.width == r.width && this.height == r.height;
-    }
-}
-//exercise 3
-class Square extends Rectangle {
-    public Square (int side) {
-        super(side,side);
-    }
+interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
 }
 
-public class Main {
-    public static void main(String[] args) {
+class Point implements Movable {
+    int x,y;
+    public Point (int a, int b) {
+        this.x = a;
+        this.y = b;
+    }
+    public void moveUp() { y++; }
 
-        Object o1 = new Rectangle(5, 10);
-        Object o2 = new Rectangle(15, 15);
-        Object o3 = new Square(15);
+    public void moveDown() { y--; }
 
-        System.out.println("Objects are identical: " + o1.equals(o2));
-        System.out.println("Objects are identical: " + o1.equals(o3));
-        System.out.println("Objects are identical: " + o2.equals(o3));
+    public void moveLeft() { x--; }
+
+    public void moveRight() { x++; }
+
+    public void print() {
+    System.out.println("(" + x + ", " + y + ")");
     }
 }
 
+class Circle implements Movable {
+    Point center;
+    int r;
+
+        public Circle(Point center, int radius) {
+        this.center = center;
+        this.r = radius;
+    }
+
+    public void moveUp() { center.moveUp(); }
+
+    public void moveDown() { center.moveDown(); }
+
+    public void moveLeft() { center.moveLeft(); }
+
+    public void moveRight() { center.moveRight(); }
+
+    public void Print() {
+        System.out.print("Center: ");
+        center.print();
+        System.out.println("Radius: " + r);
+    }
+
+}
+//checking whether it works
+class Main {
+    public static void main(String args[]) {
+      Point p1 = new Point(5,7);
+      Circle c1 = new Circle(p1,3);
+      c1.moveRight();
+      c1.moveDown();
+      c1.Print();
+    }
+}
